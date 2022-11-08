931bae628c0864ac9505b5096d368a242f40bfb6
63 rubles <sixtythreerubles@gmail.com>
2022-11-08 18:13:29 +0300

    // 8'th hometask -- done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,39 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
+            if (action.payload === 'up') {
+                state.sort((a, b) => a.name.localeCompare(b.name))
+            }
+            if (action.payload === 'down') {
+            state.sort((a, b) => b.name.localeCompare(a.name))
+            }
             return state // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(a => a.age > action.payload)
+            // need to fix
         }
         default:
             return state
     }
+}
+
+export const sortUsersAC = () => {
+    return {
+        type: 'sort',
+        payload: {
+            up: 'up',
+            down: 'down'
+        }
+    } as const
+}
+
+export const checkUsersAC = () => {
+    return {
+        type: 'check',
+        payload: 18
+    } as const
 }
